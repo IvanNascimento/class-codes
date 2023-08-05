f68d52feb5fd390a01d1d8a531f28374cc6e388a
IvanNascimento <ivanluis431@gmail.com>
2023-08-05 15:54:25 -0300

    att & add

--- JavaScript/Linguagem de Script/atividade1/index.js
+++ JavaScript/Linguagem de Script/atividade1/index.js
@@ -4,7 +4,9 @@
  * @returns Área do quadrado
  */
 function Q1(lado) {
-  return lado * lado;
+  let result = lado * lado;
+  console.log(`Área: ${result}`);
+  return result;
 }
 
 /**
@@ -13,7 +15,9 @@ function Q1(lado) {
  * @returns Perímetro do quadrado
  */
 function Q2(lado) {
-  return lado * 4;
+  let result = lado * 4;
+  console.log(`Perímetro: ${result}`);
+  return result;
 }
 
 /**
@@ -23,7 +27,9 @@ function Q2(lado) {
  * @returns Perímetro do retângulo
  */
 function Q3(comprimento, largura) {
-  return comprimento * 2 + largura * 2;
+  let result = comprimento * 2 + largura * 2;
+  console.log(`Perímetro: ${result}`);
+  return result;
 }
 
 /**
@@ -33,7 +39,9 @@ function Q3(comprimento, largura) {
  * @returns Área do retângulo
  */
 function Q4(comprimento, largura) {
-  return comprimento * largura;
+  let result = comprimento * largura;
+  console.log(`Área: ${result}`);
+  return result;
 }
 
 /**
@@ -43,7 +51,9 @@ function Q4(comprimento, largura) {
  * @returns Área do retângulo
  */
 function Q5(galinhas) {
-  return galinhas * 2 * 4;
+  let result = galinhas * 2 * 4;
+  console.log(`Dedos: ${result}`);
+  return result;
 }
 
 /**
@@ -53,7 +63,9 @@ function Q5(galinhas) {
  * @returns Área do retângulo
  */
 function Q6(vacas) {
-  return vacas * 4 * 5;
+  let result = vacas * 4 * 5;
+  console.log(`Litros: ${result}`);
+  return result;
 }
 
 /**
@@ -64,9 +76,19 @@ function Q6(vacas) {
  * @returns Área do retângulo
  */
 function Q7(x, y) {
+  console.log(`
+  \n ===== Entrada =====
+  \n X: ${x}
+  \n Y: ${y}
+  `);
   x += y;
   y = x - y;
   x = x - y;
+  console.log(`
+  \n ===== Saída =====
+  \n X: ${x}
+  \n Y: ${y}
+  `);
   return { x: x, y: y };
 }
 
@@ -77,7 +99,9 @@ function Q7(x, y) {
  * @returns Área do retângulo
  */
 function Q8(vacas) {
-  return Math.ceil(Q6(vacas) / 2);
+  let result = Math.ceil(Q6(vacas) / 2);
+  console.log(`Garrafas: ${result}`);
+  return result;
 }
 
 /**
@@ -87,8 +111,11 @@ function Q8(vacas) {
  * @returns Área do retângulo
  */
 function Q9(vacas) {
-  return Q8(vacas) * 3.0;
+  let result = Q8(vacas) * 3.0;
+  console.log(`Lucro: ${result.toFixed(2)}`);
+  return result;
 }
+
 /**
  * TODO att descrição
  * Dado o número retorna a área do retângulo
@@ -99,10 +126,12 @@ function Q9(vacas) {
 function Q10(vacas, garrafas) {
   let lucro = Q9(vacas);
   if (garrafas > 10) {
+    console.log(`Lucro (10% off): ${lucro.toFixed(2)}`);
     return lucro * 0.9;
   }
   return lucro;
 }
+
 /**
  * TODO att descrição
  * Dado o número retorna a área do retângulo
@@ -111,7 +140,9 @@ function Q10(vacas, garrafas) {
  * @returns Área do retângulo
  */
 function Q11(peso, altura) {
-  return peso / altura ** 2;
+  let result = peso / altura ** 2;
+  console.log(`IMC: ${result}`);
+  return result;
 }
 
 /**
@@ -122,7 +153,55 @@ function Q11(peso, altura) {
  * @returns Área do retângulo
  */
 function Q12(anterior, atual) {
-  let consumo = atual - anterior;
   const ilu = 15.78;
-  var banver = 21.52;
+  const precokwh = 0.80998;
+  const vermelha = 21.52;
+  const icmsper = 0.27;
+  const pisper = 0.009912;
+  const cofinsper = 0.045657;
+
+  let multa = 6.52;
+  let mora = 0.65;
+  // const amarela = 10.25;
+
+  let consumo = atual - anterior;
+  let consumoP = consumo * precokwh;
+
+  console.log(`
+  \n Total: R$ ${(consumoP + ilu + vermelha + multa + mora).toFixed(2)}
+  \n Consumo: R$ ${consumoP.toFixed(2)}
+  \n Bandeira Vermelha: R$ ${vermelha.toFixed(2)} 
+  \n Iluminação Pública: R$ ${ilu.toFixed(2)}
+  \n Juros de Mora: R$ ${mora.toFixed(2)}
+  \n Multa: R$ ${multa.toFixed(2)}
+  \n ===== ICMS =====
+  \n Consumo: R$ ${(consumoP * icmsper).toFixed(2)}
+  \n Bandeira: R$ ${(vermelha * icmsper).toFixed(2)}
+  \n ===== PIS =====
+  \n Consumo: R$ ${(consumoP * pisper).toFixed(2)}
+  \n Bandeira: R$ ${(vermelha * pisper).toFixed(2)}
+  \n ===== COFINS =====
+  \n Consumo: R$ ${(consumoP * cofinsper).toFixed(2)}
+  \n Bandeira: R$ ${(vermelha * cofinsper).toFixed(2)}`);
+
+  return {
+    total: (consumoP + ilu + vermelha + multa + mora).toFixed(2),
+    consumo: consumoP.toFixed(2),
+    bandeira: vermelha.toFixed(2),
+    iluminacao: ilu.toFixed(2),
+    juros: mora.toFixed(2),
+    multa: multa.toFixed(2),
+    icms: {
+      consumo: (consumoP * icmsper).toFixed(2),
+      bandeira: (vermelha * icmsper).toFixed(2),
+    },
+    pis: {
+      consumo: (consumoP * pisper).toFixed(2),
+      bandeira: (vermelha * pisper).toFixed(2),
+    },
+    cofins: {
+      consumo: (consumoP * cofinsper).toFixed(2),
+      bandeira: (vermelha * cofinsper).toFixed(2),
+    },
+  };
 }
