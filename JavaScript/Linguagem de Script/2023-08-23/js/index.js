f060991e448a275683b885044e66c8fcdd72ed3e
IvanNascimento <ivanluis431@gmail.com>
2023-08-24 15:15:13 -0300

    att

--- JavaScript/Linguagem de Script/2023-08-23/js/index.js
+++ JavaScript/Linguagem de Script/2023-08-23/js/index.js
@@ -1,9 +1,47 @@
 function log(string) {
-  document.getElementById("saida").textContent = string;
+  document.getElementById("saida").textContent += `${string}\n`;
 }
 
 function chamei() {
-  let cpf = document.getElementById("cpf");
+  /**
+   * @type String
+   */
+  let cpf = document.getElementById("cpf").value; // -> 11144477705
 
-  log("Informe um CPF válido");
+  if (cpf.length != 11) {
+    log("Informe um CPF válido.");
+    return 0;
+  }
+
+  let base = cpf.slice(0, 9);
+
+  let soma1 = 0,
+    soma2 = 0,
+    dig1,
+    dig2,
+    resto;
+
+  for (let i = 0; i < 9; i++) {
+    soma1 += parseInt(cpf[i]) * (10 - i);
+  }
+
+  resto = soma1 % 11;
+  dig1 = resto < 2 ? 0 : 11 - resto;
+  base += dig1;
+
+  for (let i = 0; i < 10; i++) {
+    soma2 += parseInt(base[i]) * (11 - i);
+  }
+
+  resto = soma2 % 11;
+  dig2 = resto < 2 ? 0 : 11 - resto;
+  base += dig2;
+
+  out = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+
+  if (base == cpf) {
+    log(`O CPF "${out}" é válido.`);
+  } else {
+    log(`O CPF "${out}" é inválido.`);
+  }
 }
